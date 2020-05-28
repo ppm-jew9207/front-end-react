3a33c6786941789b04ca1b5c815ffa6df2cb2179
MariuzM <mariuzz7789@gmail.com>
2020-05-28 19:29:52 +0100

    simple fix

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -5,19 +5,23 @@ const LoginComponent = () => {
   return (
     <div className="form-container">
       <h2>Login</h2>
+
       <form id="login">
         <div className="input-container">
           <UserIcon />
           <input className="input-field" type="text" name="username" placeholder="Username" />
         </div>
+
         <div className="input-container">
           <LockIcon />
           <input className="input-field" type="password" name="password" placeholder="Password" />
         </div>
+
         <div className="input-container">
           <input className="input-checkbox" type="checkbox" name="remember" id="remember" />
           <label htmlFor="remember">Remember me</label>
         </div>
+
         <div>
           <input className="input-button" type="submit" value="Login" />
           <a className="lost-password" href="#">
