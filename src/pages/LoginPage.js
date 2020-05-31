908a64e794ba90f23b571a428d632c726aabdd65
Oskaras Razma <oskaras131@gmail.com>
2020-06-01 00:30:32 +0300

    Test 3 commit

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,6 +5,7 @@ import RegisterComponent from '../components/RegisterComponent'
 const LoginPage = () => {
   return (
     <div className="flex-container">
+      I'm better ;) 
       <LoginComponent />
       <div className="separator">
         <label>OR</label>
